1c13730453a69900143311545598fa96e41f8aef
DevExpressExampleBot <fakeEmail>
2022-12-17 04:53:10 +0400

    Source auto update [skip ci]

--- VB/PopUpMenuShowingEvent/MainWindow.xaml.vb
+++ VB/PopUpMenuShowingEvent/MainWindow.xaml.vb
@@ -1,59 +1,59 @@
-﻿Imports DevExpress.Xpf.Bars
+Imports DevExpress.Xpf.Bars
 Imports System.Windows
 Imports DevExpress.Xpf.Scheduling
-Imports System
-Imports System.Linq
 
 Namespace PopUpMenuShowingEvent
-	''' <summary>
-	''' Interaction logic for MainWindow.xaml
-	''' </summary>
-	Partial Public Class MainWindow
-		Inherits Window
 
-		Private myMenuItem As BarButtonItem
-		Public Sub New()
-			InitializeComponent()
-		End Sub
-		#Region "#PopUpMenuShowing"
-		Private Sub scheduler_PopupMenuShowing(ByVal sender As Object, ByVal e As PopupMenuShowingEventArgs)
-			'Customize the cell context menu:
-			If e.MenuType = ContextMenuType.CellContextMenu Then
-				Dim menu As PopupMenu = CType(e.Menu, PopupMenu)
+    ''' <summary>
+    ''' Interaction logic for MainWindow.xaml
+    ''' </summary>
+    Public Partial Class MainWindow
+        Inherits Window
 
-				'Change the "New All Day Event" item's caption to "Create All-Day Appointment":
-				For i As Integer = 0 To menu.Items.Count - 1
-					Dim menuItem As BarItem = TryCast(menu.Items(i), BarItem)
-					If menuItem IsNot Nothing Then
-						If menuItem IsNot Nothing AndAlso menuItem.Content.ToString() = "New All Day Event" Then
-							menuItem.Content = "Create All-Day Appointment"
-							Exit For
-						End If
-					End If
-				Next i
-				'Add new menu item: 
-				If Not menu.Items.Contains(myMenuItem) Then
-					CreateNewItem()
-					menu.Items.Add(myMenuItem)
-				End If
-			End If
-		End Sub
-		#End Region ' #PopUpMenuShowing
+        Private myMenuItem As BarButtonItem
 
+        Public Sub New()
+            Me.InitializeComponent()
+        End Sub
 
-		#Region "#NewMenuItem"
-		Private Sub CreateNewItem()
-			'Create a new menu item: 
-			myMenuItem = New BarButtonItem()
+#Region "#PopUpMenuShowing"
+        Private Sub scheduler_PopupMenuShowing(ByVal sender As Object, ByVal e As PopupMenuShowingEventArgs)
+            'Customize the cell context menu:
+            If e.MenuType = ContextMenuType.CellContextMenu Then
+                Dim menu As PopupMenu = CType(e.Menu, PopupMenu)
+                'Change the "New All Day Event" item's caption to "Create All-Day Appointment":
+                For i As Integer = 0 To menu.Items.Count - 1
+                    Dim menuItem As BarItem = TryCast(menu.Items(i), BarItem)
+                    If menuItem IsNot Nothing Then
+                        If menuItem IsNot Nothing AndAlso Equals(menuItem.Content.ToString(), "New All Day Event") Then
+                            menuItem.Content = "Create All-Day Appointment"
+                            Exit For
+                        End If
+                    End If
+                Next
 
-			myMenuItem.Name = "customItem"
-			myMenuItem.Content = "Item Added at Runtime"
-			AddHandler myMenuItem.ItemClick, AddressOf customItem_ItemClick
-		End Sub
-		Private Sub customItem_ItemClick(ByVal sender As Object, ByVal e As ItemClickEventArgs)
-			' Implement a custom action. 
-			MessageBox.Show(String.Format("{0} is clicked", e.Item.Name))
-		End Sub
-		#End Region ' #NewMenuItem
-	End Class
+                'Add new menu item: 
+                If Not menu.Items.Contains(myMenuItem) Then
+                    CreateNewItem()
+                    menu.Items.Add(myMenuItem)
+                End If
+            End If
+        End Sub
+
+#End Region  ' #PopUpMenuShowing
+#Region "#NewMenuItem"
+        Private Sub CreateNewItem()
+            'Create a new menu item: 
+            myMenuItem = New BarButtonItem()
+            myMenuItem.Name = "customItem"
+            myMenuItem.Content = "Item Added at Runtime"
+            AddHandler myMenuItem.ItemClick, New ItemClickEventHandler(AddressOf customItem_ItemClick)
+        End Sub
+
+        Private Sub customItem_ItemClick(ByVal sender As Object, ByVal e As ItemClickEventArgs)
+            ' Implement a custom action. 
+            MessageBox.Show(String.Format("{0} is clicked", e.Item.Name))
+        End Sub
+#End Region  ' #NewMenuItem
+    End Class
 End Namespace
