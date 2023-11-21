83c95938a61634c2e68ab5e2159f887894bf9879
DevExpressExampleBot <fakeEmail>
2023-11-21 17:16:15 +0400

    Source auto update [skip ci]

--- VB/PopUpMenuShowingEvent/MainWindow.xaml.vb
+++ VB/PopUpMenuShowingEvent/MainWindow.xaml.vb
@@ -1,36 +1,37 @@
-﻿Imports DevExpress.Xpf.Bars
+Imports DevExpress.Xpf.Bars
 Imports System.Windows
 Imports DevExpress.Xpf.Scheduling
-Imports System
-Imports System.Linq
 
 Namespace PopUpMenuShowingEvent
+
     ''' <summary>
     ''' Interaction logic for MainWindow.xaml
     ''' </summary>
-    Partial Public Class MainWindow
+    Public Partial Class MainWindow
         Inherits Window
 
         Private myMenuItem As BarButtonItem
+
         Public Sub New()
-            InitializeComponent()
+            Me.InitializeComponent()
         End Sub
-        #Region "#PopUpMenuShowing"
+
+#Region "#PopUpMenuShowing"
         Private Sub scheduler_PopupMenuShowing(ByVal sender As Object, ByVal e As PopupMenuShowingEventArgs)
             'Customize the cell context menu:
             If e.MenuType = ContextMenuType.CellContextMenu Then
                 Dim menu As PopupMenu = CType(e.Menu, PopupMenu)
-
                 'Change the "New All Day Event" item's caption to "Create All-Day Appointment":
                 For i As Integer = 0 To menu.Items.Count - 1
                     Dim menuItem As BarItem = TryCast(menu.Items(i), BarItem)
                     If menuItem IsNot Nothing Then
-                        If menuItem IsNot Nothing AndAlso menuItem.Content.ToString() = "New All Day Event" Then
+                        If menuItem IsNot Nothing AndAlso Equals(menuItem.Content.ToString(), "New All Day Event") Then
                             menuItem.Content = "Create All-Day Appointment"
                             Exit For
                         End If
                     End If
-                Next i
+                Next
+
                 'Add new menu item: 
                 If Not menu.Items.Contains(myMenuItem) Then
                     CreateNewItem()
@@ -38,22 +39,21 @@ Namespace PopUpMenuShowingEvent
                 End If
             End If
         End Sub
-        #End Region ' #PopUpMenuShowing
 
-
-        #Region "#NewMenuItem"
+#End Region  ' #PopUpMenuShowing
+#Region "#NewMenuItem"
         Private Sub CreateNewItem()
             'Create a new menu item: 
             myMenuItem = New BarButtonItem()
-
             myMenuItem.Name = "customItem"
             myMenuItem.Content = "Item Added at Runtime"
-            AddHandler myMenuItem.ItemClick, AddressOf customItem_ItemClick
+            AddHandler myMenuItem.ItemClick, New ItemClickEventHandler(AddressOf customItem_ItemClick)
         End Sub
+
         Private Sub customItem_ItemClick(ByVal sender As Object, ByVal e As ItemClickEventArgs)
             ' Implement a custom action. 
             MessageBox.Show(String.Format("{0} is clicked", e.Item.Name))
         End Sub
-        #End Region ' #NewMenuItem
+#End Region  ' #NewMenuItem
     End Class
 End Namespace
